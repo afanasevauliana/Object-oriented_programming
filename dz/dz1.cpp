ccbd2252ad6b3a93e546740b67027c8586f722aa
afanasevauliana <afanasevauliana946@gmail.com>
2025-03-13 23:47:06 +0300

    dz1

--- dz/dz1.cpp
+++ dz/dz1.cpp
@@ -33,6 +33,70 @@ class List {
             temp->next = new SinglyLinkedList(data);
         }
     }
+    void print() {
+        SinglyLinkedList* temp = head;
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    void deletElement(string key) {
+        if (!head) {
+            cout << "List is empty";
+            return;
+        }
+        SinglyLinkedList* temp = head;
+        while (temp->next != nullptr and temp->next->data != key) {
+            temp = temp->next;
+        }
+        if (temp->next == nullptr) {
+            cout << "Element not found" << endl;
+            return;
+        }
+        SinglyLinkedList* deleter = temp->next;
+        temp->next = temp->next->next;
+        delete deleter;
+    }
+    void addElem(string key, string data) {
+        if (!head) {
+            cout << "List is empty";
+            return;
+        }
+        SinglyLinkedList* temp = head;
+        while (temp != nullptr and temp->data != key) {
+            temp = temp->next;
+        }
+        if (temp == nullptr) {
+            cout << "Element not found" << endl;
+            return;
+        }
+        SinglyLinkedList* adder = new SinglyLinkedList(data);
+        adder->next = temp->next;
+        temp->next = adder;
+    }
+    ~List() {
+        while (head) {
+            SinglyLinkedList* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
 
-
+int main() {
+    List a;
+    for (int i{1}; i < 11; i++) {
+        a.pushQueue(to_string(i));
+    }
+    a.print();
+    a.pushStack("0");
+    a.print();
+    a.deletElement("8");
+    a.deletElement("10");
+    a.print();
+    a.deletElement("10");
+    a.addElem("8", "9");
+    a.addElem("9", "10");
+    a.print();
 }
